2338cd1dd69b8f82f24b5aa7e81a399ec4277c73
Natalye Cavalieri <cavalieridev@gmail.com>
2024-05-24 14:38:41 +0200

    add pages and hooks

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -4,7 +4,7 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai"
 export default function Button({icon : Icon, img, value, title, $loading = false, ...rest}){
   return (
     <Container $loading={$loading} {...rest}>
-      {img && <img src={img} alt="Pedir um prato" />}
+      {img && <img src={img} alt="Solicitar um prato" />}
       {$loading ? <AiOutlineLoading3Quarters /> : title}
       {Icon && <Icon />}
       {value && value}
